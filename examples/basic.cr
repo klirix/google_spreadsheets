0d5218ec8df7eecd57dd1f340ed16c88c9c08388
Askhat Saiapov <klirikus@gmail.com>
2021-03-07 04:08:07 +0300

    add batch and filter overloads to all methods

--- examples/basic.cr
+++ examples/basic.cr
@@ -12,8 +12,15 @@ sheet = spreadsheet.sheet("Sheet1")
 
 pp sheet.get("a1:b10")
 
-pp sheet.batchGet(["a1:a10", "b1:b10"])
+pp sheet.get(["a1:a10", "b1:b10"])
 
-pp sheet.append({"name" => "Joe", "age" => 10})
+pp sheet.get([RangeDataFilter.new(a1Range: "a1:b20")])
+
+sheet.append({"name" => "Joe", "age" => 10})
+
+sheet.update([ValueRange.new("b6", [["JORG"]])])
 sheet.update("a6", [[12]])
-sheet.clear("a6:b6")
+sheet.clear("a7:b7")
+sheet.clear(["a8:b8"])
+sheet.clear([RangeDataFilter.new(a1Range: "a9:b9")])
+sheet.update([DataFilterValueRange.new(RangeDataFilter.new("a8:b9"), [[12, "JORG"]])])
--- src/google_spreadsheets.cr
+++ src/google_spreadsheets.cr
@@ -22,38 +22,92 @@ module GoogleSpreadsheets
 
   alias CellInput = String | Int64 | Int32 | Bool | Nil
 
-  struct ValueRange
+  abstract struct ValueRangeData
     include JSON::Serializable
-    property range : String
-    @[JSON::Field(key: "majorDimension")]
-    property major_dimension : MajorDimension
     property values : Array(Array(CellInput))
+  end
 
-    def initialize(@range, values, @major_dimension = MajorDimension::ROWS)
+  struct ValueRange < ValueRangeData
+    property range : String
+    property majorDimension : MajorDimension
+
+    def initialize(@range, values, @majorDimension = MajorDimension::ROWS)
       @values = values.map &.map &.as(CellInput)
     end
+  end
+
+  struct DataFilterValueRange < ValueRangeData
+    property dataFilter : DataFilter
+    property majorDimension : MajorDimension
+
+    def initialize(@dataFilter, values, @majorDimension = MajorDimension::ROWS)
+      @values = values.map &.map &.as(CellInput)
+    end
+  end
+
+  abstract struct DataFilter
+    include JSON::Serializable
+  end
+
+  struct RangeDataFilter < DataFilter
+    property a1Range : String
+    def initialize(@a1Range)
+    end
+  end
+
+  struct GridRangeDataFilter < DataFilter
+    property gridRange : GridRange
+    def initialize(@gridRange)
+    end
+  end
+
+  struct GridRange
+    include JSON::Serializable
+    @sheetId : Int64
+    @startRowIndex : Int64
+    @endRowIndex : Int64
+    @startColumnIndex : Int64
+    @endColumnIndex : Int64
+
+    def initialize(@sheetId, @startRowIndex, @endRowIndex, @startColumnIndex, @endColumnIndex)
+    end
+  end
+
+  struct MatchedValueRange
+    include JSON::Serializable
+
+    property valueRange : ValueRange
+  end
+
+  struct UpdateValuesByDataFilterResponse
+    include JSON::Serializable
+
+    getter updatedRange : String
+    getter updatedRows : Int64
+    getter updatedColumns : Int64
+    getter updatedCells : Int64
+    getter updatedData : ValueRange
+  end
+
+  struct UpdateValuesResponse
+    include JSON::Serializable
+
+    getter spreadsheetId : String
+    getter updatedRange : String
+    getter updatedRows : Int64
+    getter updatedColumns : Int64
+    getter updatedCells : Int64
+    getter updatedData : ValueRange
   end
 
   enum MajorDimension
     ROWS
     COLUMNS
-
-    def to_json(io)
-      io << "\""
-      io << to_s(self)
-      io << "\""
-    end
   end
 
   enum ValueInputOption
     RAW
     USER_ENTERED
-
-    def to_json(io)
-      io << "\""
-      io << to_s(self)
-      io << "\""
-    end
   end
 
   class AuthException < Exception
@@ -101,9 +155,7 @@ module GoogleSpreadsheets
             "/values/#{sheeted_range}:append" \
             "?valueInputOption=USER_ENTERED"
       payload = ValueRange.new(sheeted_range, [data], major_dimension).to_json
-      headers = HTTP::Headers.new
-      headers["Authorization"] = "Bearer #{@spreadsheet.oauth_token}"
-      res = @spreadsheet.client.post url, body: payload, headers: headers
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
     end
 
     def append(data : Hash(String, CellInput), range : String = "A1")
@@ -131,16 +183,35 @@ module GoogleSpreadsheets
       end
     end
 
-    def clear(range)
+    def clear(range : String)
       before_updating
       sheeted_range = range.includes?("!") ? range : "#{title}!#{range}"
-      url = "/v4/spreadsheets/#{@spreadsheet.id}/values/#{sheeted_range}:clear?key=#{@spreadsheet.api_key}"
-      res = @spreadsheet.client.post url
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values/#{sheeted_range}:clear"
+      res = @spreadsheet.client.post url, headers: @spreadsheet.auth_headers
+    end
+
+    def clear(ranges : Array(String))
+      before_updating
+      payload = {
+        ranges: ranges.map do |range|
+          range.includes?("!") ? range : "#{title}!#{range}"
+        end
+      }.to_json
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values:batchClear"
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
+    end
+
+    def clear(data_filters : Array(DataFilter))
+      before_updating
+      payload = { dataFilters: data_filters }.to_json
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values:batchClearByDataFilter"
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
     end
 
     def update(range, data, *,
         major_dimension = MajorDimension::ROWS,
         value_input_option = ValueInputOption::USER_ENTERED)
+      sheeted_range = range.includes?("!") ? range : "#{title}!#{range}"
       update(ValueRange.new(sheeted_range, data, major_dimension),
         major_dimension: major_dimension,
         value_input_option: value_input_option
@@ -151,14 +222,40 @@ module GoogleSpreadsheets
       major_dimension = MajorDimension::ROWS,
       value_input_option = ValueInputOption::USER_ENTERED)
       before_updating
-      sheeted_range = range.includes?("!") ? range : "#{title}!#{range}"
       url = "/v4/spreadsheets/" \
-            "#{@spreadsheet.id}/values/#{sheeted_range}" \
-            "?key=#{@spreadsheet.api_key}&valueInputOption=#{value_input_option.to_s}"
+            "#{@spreadsheet.id}/values/#{value_range.range}" \
+            "?key=#{@spreadsheet.api_key}&valueInputOption=#{value_input_option.to_s}&includeValuesInResponse=true"
       payload = value_range.to_json
-      headers = HTTP::Headers.new
-      headers["Authorization"] = "Bearer #{@spreadsheet.oauth_token}"
-      res = @spreadsheet.client.put url, body: payload, headers: headers
+      res = @spreadsheet.client.put url, body: payload, headers: @spreadsheet.auth_headers
+      UpdateValuesResponse.from_json(res.body)
+    end
+
+    def update(value_ranges : Array(ValueRange), *,
+      major_dimension = MajorDimension::ROWS,
+      value_input_option = ValueInputOption::USER_ENTERED)
+      before_updating
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values:batchUpdate"
+      payload = {
+        valueInputOption: value_input_option,
+        data: value_ranges,
+        includeValuesInResponse: true,
+      }.to_json
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
+      Array(UpdateValuesResponse).from_json(res.body, "responses")
+    end
+
+    def update(value_ranges : Array(DataFilterValueRange), *,
+      major_dimension = MajorDimension::ROWS,
+      value_input_option = ValueInputOption::USER_ENTERED)
+      before_updating
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values:batchUpdateByDataFilter"
+      payload = {
+        valueInputOption: value_input_option,
+        data: value_ranges,
+        includeValuesInResponse: true,
+      }.to_json
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
+      Array(UpdateValuesByDataFilterResponse).from_json(res.body, "responses")
     end
 
     def get(range, major_dimension = MajorDimension::ROWS)
@@ -171,7 +268,7 @@ module GoogleSpreadsheets
       ValueRange.from_json(res.body)
     end
 
-    def batchGet(ranges, major_dimension = MajorDimension::ROWS)
+    def get(ranges : Array(String), major_dimension = MajorDimension::ROWS)
       before_reading
       queriable_ranges = ranges.map do |range|
         "ranges=#{
@@ -181,9 +278,21 @@ module GoogleSpreadsheets
       url = "/v4/spreadsheets/" \
             "#{@spreadsheet.id}/values:batchGet" \
             "?#{queriable_ranges}" \
-            "&key=#{@spreadsheet.api_key}&majorDimension=#{major_dimension.to_s.upcase}"
+            "&key=#{@spreadsheet.api_key}&majorDimension=#{major_dimension.to_s}"
       res = @spreadsheet.client.get url
       Array(ValueRange).from_json(res.body, "valueRanges")
+    end
+
+    def get(filters : Array(DataFilter), major_dimension = MajorDimension::ROWS)
+      before_reading
+      url = "/v4/spreadsheets/#{@spreadsheet.id}/values:batchGetByDataFilter"
+      payload = {
+        dataFilters: filters,
+        majorDimension: major_dimension.to_s
+      }.to_json
+      res = @spreadsheet.client.post url, body: payload, headers: @spreadsheet.auth_headers
+      pp res
+      Array(MatchedValueRange).from_json(res.body, "valueRanges")
     end
   end
 
@@ -195,6 +304,12 @@ module GoogleSpreadsheets
     getter oauth_token : String | Nil
     def oauth_token=(token)
       @oauth_token = token.includes?("Bearer") ? token.split(" ")[1] : token
+    end
+
+    def auth_headers
+      headers = HTTP::Headers.new
+      headers["Authorization"] = "Bearer #{oauth_token}"
+      headers
     end
 
     def initialize(@id, @api_key, token : String? = nil)
